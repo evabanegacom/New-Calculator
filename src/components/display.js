5c68317b62e8dee06acd85eea365209629304c4b
Precious chiedu <udegbue69@gmail.com>
2021-09-13 22:55:17 +0100

    created calculator display

---
+++ src/components/display.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Display = ({ result, operation }) => (
+  <div className="calc-display">
+    {`${result} ${operation || ''}`}
+  </div>
+);
+
+Display.propTypes = {
+  result: PropTypes.string,
+  operation: PropTypes.string,
+
+};
+
+Display.defaultProps = {
+  result: '0',
+  operation: '',
+
+};
+
+export default Display;
